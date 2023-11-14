ab7c5842daf8b40b7976b307c7f485606f2bb058
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-11-14 22:21:58 +0200

    git init

---
+++ AnalizerClassLibrary.Tests/AnalizerClassTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using AnalaizerClassLibrary;
+using Xunit;
+
+namespace AnalizerClassLibrary.Tests;
+
+public class AnalizerClassTests
+{
+    private readonly DbManager _dbManager;
+
+    public AnalizerClassTests()
+    {
+        _dbManager = new DbManager();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(3)]
+    public void CreateStack_ShouldReturnCorrectResult_WhenGivenSimpleArithmeticExpression(int id)
+    {
+        TestAnalaizer(id);
+    }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(2)]
+    public void CreateStack_ShouldReturnCorrectResult_WhenGivenExpressionWithBrackets(int id)
+    {
+        TestAnalaizer(id);
+    }
+
+    [Theory]
+    [InlineData(4)]
+    [InlineData(6)]
+    public void CreateStack_ShouldReturnCorrectResult_WhenGivenExpressionWithSingleNumber(int id)
+    {
+        TestAnalaizer(id);
+    }
+
+    private void TestAnalaizer(int id)
+    {
+        //Arrange
+        ExpressionData expressionData = _dbManager.GetExpressionById(id);
+        AnalaizerClass.expression = expressionData.Expression;
+        ArrayList expectedArray = new(expressionData.ExpectedResult.Split(','));
+
+        //Act
+        ArrayList result = AnalaizerClass.CreateStack();
+
+        //Assert
+        Assert.Equal(result, expectedArray);
+    }
+}
---
+++ AnalizerClassLibrary.Tests/DbManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+namespace AnalizerClassLibrary.Tests;
+
+public class ExpressionData
+{
+    public string Expression { get; set; }
+    public string ExpectedResult { get; set; }
+}
+
+public class DbManager
+{
+    private const string ConnectionString =
+        @"server=(localdb)\MSSQLLocalDB;database=ArithmeticExpressionsDB;trusted_connection=true;Integrated Security=True";
+
+    public ExpressionData GetExpressionById(int id)
+    {
+        ExpressionData expressionData = new();
+
+        using SqlConnection connection = new(ConnectionString);
+        
+        connection.Open();
+        string query = "SELECT Expression, ExpectedResult FROM ArithmeticExpressions WHERE Id = @Id";
+        
+        using SqlCommand command = new(query, connection);
+        command.Parameters.AddWithValue("@Id", id);
+        
+        using SqlDataReader reader = command.ExecuteReader();
+        if (reader.Read())
+        {
+            expressionData.Expression = reader["Expression"].ToString();
+            expressionData.ExpectedResult = reader["ExpectedResult"].ToString();
+        }
+        return expressionData;
+    }
+}
